99a7b26b33cd32206a18937eec3e00ccaa442b0a
Fernando Guevara <luisfernandoguevara95@gmail.com>
2021-02-16 11:12:57 -0600

    (Add): Header

---
+++ src/components/layout/Header/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Title = styled.h1`
+  margin: 24px 30px 0;
+  font-size: 20px;
+  font-weight: 600;
+  color: #12153D;
+`;
--- src/router/HomeRouter.js
+++ src/router/HomeRouter.js
@@ -3,12 +3,14 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import { PopularPage } from "../pages/PopularPage";
 import { TopRatedPage } from "../pages/TopRatedPage";
 import { AiringTodayPage } from "../pages/AiringTodayPage";
+import { Header } from "../components/layout/Header";
 import { Navbar } from "../components/layout/Navbar";
 import { Filter } from "../components/layout/Filter";
 
 export const HomeRouter = () => {
   return (
     <div>
+      <Header />
       <Navbar />
       <Filter />
       <Switch>
