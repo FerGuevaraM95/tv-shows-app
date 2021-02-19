8c0074fe7662a3f2dc61197fc81d1cc886b5443c
Fernando Guevara <luisfernandoguevara95@gmail.com>
2021-02-18 23:06:19 -0600

    (Add): Set vavorites in localstorage

--- src/components/DetailHeader/index.js
+++ src/components/DetailHeader/index.js
@@ -10,21 +10,55 @@ import {
   PopularitySquad,
   Span,
   SpanLight,
-  SubSpan
+  SubSpan,
 } from "./styles";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
 
-export const DetailHeader = ({ name, poster, vote, voteCount, popularity }) => {
+export const DetailHeader = ({
+  id,
+  name,
+  poster,
+  vote,
+  voteCount,
+  popularity,
+}) => {
+
+  // Localstorage
+  const [favorites, setFavorites] = useLocalStorage(
+    "favorites",
+    JSON.stringify([])
+  );
+
+  // Hanadle favorite in localstorage
+  const handleFavorite = () => {
+    const arrFavorites = JSON.parse(favorites);
+    const newFavorites = arrFavorites.find((element) => element === id)
+      ? arrFavorites.filter((element) => element !== id)
+      : [...arrFavorites, id];
+    setFavorites(JSON.stringify(newFavorites));
+  };
+
+  // print star color
+  const printStar = () => {
+    return JSON.parse(favorites).find((element) => element === id)
+      ? star
+      : starGray;
+  };
+
   return (
     <Container>
       <Image src={poster} alt={name} loading="lazy" />
       <InfoCard>
         <ValueContainer>
           <Star src={star} alt="star" />
-          <Span>{vote}<SubSpan>/10</SubSpan></Span>
+          <Span>
+            {vote}
+            <SubSpan>/10</SubSpan>
+          </Span>
           <SpanLight>{voteCount}</SpanLight>
         </ValueContainer>
         <ValueContainer>
-          <FavoriteStar src={starGray} alt="star" />
+          <FavoriteStar onClick={handleFavorite} src={printStar()} alt="star" />
           <Span>Favorito</Span>
         </ValueContainer>
         <ValueContainer>
